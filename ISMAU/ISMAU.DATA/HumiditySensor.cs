3f68fcac1d9ea904718235172f8796f4130da963
Todor Todorov <spindisamas@gmail.com>
2019-12-09 02:08:11 +0200

    Adding abstract class <Sensors>

---
+++ ISMAU/ISMAU.DATA/HumiditySensor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISMAU.DATA
+{
+    public class HumiditySensor : Sensor<float>
+    {
+        public override float GetData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ISMAU/ISMAU.DATA/Sensor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISMAU.DATA
+{
+    public class Location
+    {
+        public Location(double latitude, double longtitude)
+        {
+            Latitude = latitude;
+            Longtitude = longtitude;
+        }
+
+        public Location() : this(0d, 0d)
+        {
+        }
+
+        public double Latitude { get; set; }
+        public double Longtitude { get; set; }
+    }
+
+    public class RangeBoundaries<T>
+        where T : struct
+    {
+        public T Min { get; set; }
+        public T Max { get; set; }
+    }
+
+    public abstract class Sensor<T>
+        where T : struct
+    {
+        public string name;
+        public string description;
+
+        public string Name
+        {
+            get => default;
+            set => name = value != null ? value : "NoName";
+        }
+        public string Description
+        {
+            get => default;
+            set => description = value != null ? value : "NoDescription";
+        }
+        public int PollingInterval { get; set; }
+        public Location Location { get; set; }
+        public RangeBoundaries<T> AcceptableRange { get; set; }
+        public float TickOff { get; set; }
+
+
+        public abstract T GetData();
+    }
+}
