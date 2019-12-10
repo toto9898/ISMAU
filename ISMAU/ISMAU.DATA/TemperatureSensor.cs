200041b387cd3ec15af331b76da790e75345194a
Todor Todorov <spindisamas@gmail.com>
2019-12-10 18:07:39 +0200

    added TemperatureSensor class

---
+++ ISMAU/ISMAU.DATA/TemperatureSensor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISMAU.DATA
+{
+    public class TemperatureSensor : Sensor<double>
+    {
+        public override double GetData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
