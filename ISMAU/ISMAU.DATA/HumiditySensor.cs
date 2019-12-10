706c537c635c8a6a6119bf0e0a579b5a8ba06645
Todor Todorov <spindisamas@gmail.com>
2019-12-10 18:09:02 +0200

    added data members for the sensors value

--- ISMAU/ISMAU.DATA/HumiditySensor.cs
+++ ISMAU/ISMAU.DATA/HumiditySensor.cs
@@ -8,6 +8,21 @@ namespace ISMAU.DATA
 {
     public class HumiditySensor : Sensor<float>
     {
+        private static double INVALID_VALUE = -1.0;
+        private double humidity;
+
+        public double Humidity
+        {
+            get => humidity;
+            set
+            {
+                if (value >= AcceptableRange.Min && value <= AcceptableRange.Max)
+                    humidity = value;
+                else
+                    humidity = INVALID_VALUE;
+            }
+        }
+
         public override float GetData()
         {
             throw new NotImplementedException();
--- ISMAU/ISMAU.DATA/TemperatureSensor.cs
+++ ISMAU/ISMAU.DATA/TemperatureSensor.cs
@@ -8,6 +8,21 @@ namespace ISMAU.DATA
 {
     public class TemperatureSensor : Sensor<double>
     {
+        private static double INVALID_VALUE = -239.0;
+        private double degrees;
+
+        public double Degrees
+        {
+            get => degrees;
+            set
+            {
+                if (value >= AcceptableRange.Min && value <= AcceptableRange.Max)
+                    degrees = value;
+                else
+                    degrees = INVALID_VALUE;
+            }
+        }
+
         public override double GetData()
         {
             throw new NotImplementedException();
