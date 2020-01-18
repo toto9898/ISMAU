db326b32e9964f7e4f9dbd0ab3dddfab66290045
Todor Todorov <spindisamas@gmail.com>
2020-01-18 21:06:47 +0200

    added function AddSensor, for adding new sensors

--- ISMAU/ISMAU.FUNCTIONALITY/SensorLogic.cs
+++ ISMAU/ISMAU.FUNCTIONALITY/SensorLogic.cs
@@ -78,6 +78,66 @@ namespace ISMAU.FUNCTIONALITY
 			sensors.Serialize(databasePath);
 		}
 
+		public bool AddSensor
+		(
+			string name,
+			string description,
+			Location location,
+			float tickOff,
+			string type,
+			string boundariesMin,
+			string boundariesMax,
+			int pollingInterval = 1000)
+		{
+			bool success = true;
+
+			if (type == "DoorSensor")
+				sensors.Add(new DoorSensor(name, description, location, tickOff, pollingInterval));
+			else if (type == "ElPowerSensor" || type == "NoiseSensor")
+			{
+				success = int.TryParse(boundariesMin, out int min);
+				if (!success) return false;
+				success = int.TryParse(boundariesMax, out int max);
+				if (!success) return false;
+
+				RangeBoundaries<int> boundaries = new RangeBoundaries<int>();
+				boundaries.Min = min;
+				boundaries.Max = max;
+
+				if (type == "ElPowerSensor")
+					sensors.Add(new ElPowerSensor(name, description, location, boundaries, tickOff, pollingInterval));
+				else
+					sensors.Add(new NoiseSensor(name, description, location, boundaries, tickOff, pollingInterval));
+			}
+			else if (type == "HumiditySensor")
+			{
+				success = float.TryParse(boundariesMin, out float min);
+				if (!success) return false;
+				success = float.TryParse(boundariesMax, out float max);
+				if (!success) return false;
+
+				RangeBoundaries<float> boundaries = new RangeBoundaries<float>();
+				boundaries.Min = min;
+				boundaries.Max = max;
+
+				sensors.Add(new HumiditySensor(name, description, location, boundaries, tickOff, pollingInterval));
+			}
+			else if (type == "TemperatureSensor")
+			{
+				success = double.TryParse(boundariesMin, out double min);
+				if (!success) return false;
+				success = double.TryParse(boundariesMax, out double max);
+				if (!success) return false;
+
+				RangeBoundaries<double> boundaries = new RangeBoundaries<double>();
+				boundaries.Min = min;
+				boundaries.Max = max;
+
+				sensors.Add(new TemperatureSensor(name, description, location, boundaries, tickOff, pollingInterval));
+			}
+			return success;
+		}
+
 		#endregion
 	}
 }
